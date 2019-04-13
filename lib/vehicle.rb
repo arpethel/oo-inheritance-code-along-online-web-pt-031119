3338972dae3c9a6b80fbe976a0116ac32352ee21
Ryan Pethel <arpethel@gmail.com>
2019-04-13 23:46:25 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,10 @@
 require 'pry'
 class Vehicle
-  attr_accessor :wheel_size#, :number
+  attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size)
     @wheel_size = wheel_size
-    # @number = number
+    @wheel_number = wheel_number
   end
 
   def go
